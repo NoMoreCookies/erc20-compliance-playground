ff70640cd18f97194ba04692e483a7ca4b9037b1
Kacper <egracz123@gmail.com>
2026-04-25 13:41:31 +0200

    Started milestone 1 after successfully completing milestone 0

---
+++ src/compliance/ComplianceService.sol
@@ -0,0 +1,15 @@
+// PDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ComplianceToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("SmartToken", "SMRT"){
+        _mint(msg.sender, initialSupply);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 2;
+    }
+}
+
---
+++ test/compliance/Compliance.t.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {ComplianceToken} from "../../src/compliance/ComplianceService.sol";
+
+contract ComplianceTokenTest is Test {
+
+    ComplianceToken token;
+
+    address deployer = address(this);
+
+    address groundControl = address(0x1);
+    address majorTom = address(0x2);
+
+    uint256 constant INITIAL_SUPPLY = 10_000;
+
+    function setUp() public {
+        token = new ComplianceToken(INITIAL_SUPPLY);
+    }
+
+    function testNameIntegration() public {
+        assertEq(token.name(), "SmartToken");
+    }
+
+    function testSymbolIntegration() public view {
+        assertEq(token.symbol(), "SMRT");
+    }
+
+    function testDecimalsIntegration() public view {
+        assertEq(token.decimals(), 2);
+    }
+
+    function testTotalSupplyIntegration() public view {
+        assertEq(token.totalSupply(), INITIAL_SUPPLY);
+        assertEq(token.balanceOf(deployer), INITIAL_SUPPLY);
+    }
+
+    function testCanTransferTokens() public {
+
+        uint256 amount = 1_500; // 15 SMRT
+
+        bool success = token.transfer(groundControl, amount);
+
+        assertTrue(success);
+
+        assertEq(token.balanceOf(groundControl), amount);
+        assertEq(token.balanceOf(deployer), INITIAL_SUPPLY - amount);
+    }
+
+    function testGroundControlCanTransferTokensToMajorTom() public {
+        uint256 amountToGroundControl = 5_000; // 50.00 SMRT
+        uint256 amountToMajorTom = 1_250;   // 12.50 SMRT
+
+        bool success1 = token.transfer(groundControl, amountToGroundControl);
+        
+        assertTrue(success1);
+
+        vm.prank(groundControl);
+
+        bool success2 = token.transfer(majorTom, amountToMajorTom);
+        
+        assertTrue(success2);
+
+        assertEq(token.balanceOf(groundControl), amountToGroundControl - amountToMajorTom);
+        assertEq(token.balanceOf(majorTom), amountToMajorTom);
+    }
+
+    function testCannotTransferMoreThanBalance() public {
+
+        uint256 tooMuch = INITIAL_SUPPLY + 1;
+
+        vm.expectRevert();
+        token.transfer(groundControl, tooMuch);
+    }
+}
